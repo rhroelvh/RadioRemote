1fba1fa73e3985ec135eeeebb638420eadef980b
eothein <jens.buysse@hogent.be>
2017-06-27 15:21:49 +0200

    Migraties opgelost

--- migrations/m170627_121303_create_port_table.php
+++ migrations/m170627_121303_create_port_table.php
@@ -17,13 +17,13 @@ class m170627_121303_create_port_table extends Migration {
     public function up() {
         $this->createTable('switchPort', [
             'switchPort_id' => $this->primaryKey(),
-            'switchPort_switchId' => $this->integer(),
+            'switchPort_switchId' => $this->integer()->notNull(),
             'switchPort_speed' => $this->integer(),
             'switchPort_description' => $this->string()
         ]);
 
         //add foreignKey
-        $this->addForeignKey(self::$port_switchId_FK1, 'switchPort', 'switchPort_switchId', 'switch', 'switch_id');
+        $this->addForeignKey(self::$port_switchId_FK1, 'switchPort', 'switchPort_switchId', 'switch', 'switch_DeviceID');
     }
 
     /**
