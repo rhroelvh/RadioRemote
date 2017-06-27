246c5d61f55df46bc41cc604368440b331bc04c2
eothein <jens.buysse@hogent.be>
2017-06-27 16:06:32 +0200

    Gerelateerde data toegevoegd in Device overzicht

--- models/Device.php
+++ models/Device.php
@@ -11,34 +11,36 @@ use Yii;
  * @property integer $device_idBoat
  * @property string $device_manufacturer
  * @property string $device_model
+ * 
+ * 		
+ * @property Boat $deviceIdBoat 
+ * @property Switch $switch 
  */
-class Device extends \yii\db\ActiveRecord
-{
+class Device extends \yii\db\ActiveRecord {
+
     /**
      * @inheritdoc
      */
-    public static function tableName()
-    {
+    public static function tableName() {
         return 'device';
     }
 
     /**
      * @inheritdoc
      */
-    public function rules()
-    {
+    public function rules() {
         return [
             [['device_idBoat'], 'required'],
             [['device_idBoat'], 'integer'],
             [['device_manufacturer', 'device_model'], 'string', 'max' => 255],
+            [['device_idBoat'], 'exist', 'skipOnError' => true, 'targetClass' => Boat::className(), 'targetAttribute' => ['device_idBoat' => 'boat_id']],
         ];
     }
 
     /**
      * @inheritdoc
      */
-    public function attributeLabels()
-    {
+    public function attributeLabels() {
         return [
             'device_id' => 'Device ID',
             'device_idBoat' => 'Device Id Boat',
@@ -46,4 +48,19 @@ class Device extends \yii\db\ActiveRecord
             'device_model' => 'Device Model',
         ];
     }
+
+    /**
+     * @return \yii\db\ActiveQuery 
+     */
+    public function getDeviceIdBoat() {
+        return $this->hasOne(Boat::className(), ['boat_id' => 'device_idBoat']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery 
+     */
+    public function getSwitch() {
+        return $this->hasOne(Deviceswitch::className(), ['switch_deviceId' => 'device_id']);
+    }
+
 }
--- views/device/index.php
+++ views/device/index.php
@@ -1,35 +1,54 @@
 <?php
 
-use yii\helpers\Html;
+use app\models\Boat;
+use app\models\DeviceSearch;
+use yii\data\ActiveDataProvider;
 use yii\grid\GridView;
+use yii\helpers\ArrayHelper;
+use yii\helpers\Html;
+use yii\web\View;
 use yii\widgets\Pjax;
-/* @var $this yii\web\View */
-/* @var $searchModel app\models\DeviceSearch */
-/* @var $dataProvider yii\data\ActiveDataProvider */
+
+/* @var $this View */
+/* @var $searchModel DeviceSearch */
+/* @var $dataProvider ActiveDataProvider */
 
 $this->title = 'Devices';
 $this->params['breadcrumbs'][] = $this->title;
+
+$boten = ArrayHelper::map(Boat::find()->all(), 'boat_id', 'boat_name');
 ?>
 <div class="device-index">
 
     <h1><?= Html::encode($this->title) ?></h1>
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+    <?php // echo $this->render('_search', ['model' => $searchModel]);    ?>
 
     <p>
         <?= Html::a('Create Device', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
-<?php Pjax::begin(); ?>    <?= GridView::widget([
+    <?php Pjax::begin(); ?>    <?=
+    GridView::widget([
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
         'columns' => [
             ['class' => 'yii\grid\SerialColumn'],
-
             'device_id',
-            'device_idBoat',
+            [
+                'attribute' => 'device_idBoat',
+                //'filterType' => GridView::FILTER_SELECT2,
+                'filter' => $boten,
+                'value' => 'deviceIdBoat.boat_name',
+               // 'value' => 'klantType.klanttype_beschrijving',
+                //'filterWidgetOptions' => [
+                //    'pluginOptions' => ['allowClear' => true],
+                //],
+                //'filterInputOptions' => ['placeholder' => 'Type'],
+            ],
+            //'device_idBoat',
             'device_manufacturer',
             'device_model',
-
             ['class' => 'yii\grid\ActionColumn'],
         ],
-    ]); ?>
-<?php Pjax::end(); ?></div>
+    ]);
+    ?>
+    <?php Pjax::end(); ?></div>
